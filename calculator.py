2779ce480345cb1d35db04fd81a026c2b8a9d020
Robin Leman <robin.leman@mail.mcgill.ca>
2020-01-18 17:59:39 -0500

    modif calculator

--- calculator.py
+++ calculator.py
@@ -1,25 +1,24 @@
-d = 14776367535688
-r = 12.5*6*24*7
-
 def hr_to_btc(hr, d, r):
     ''' int, int -> float 
     Calculates the expected mine assets a according to a hash rate h
     '''
-    ghr = d * 2**32 / 600
+    ghr = d * 2**32  / ( t )  # EH/s = H * 10^18  /s
     a = hr / ghr * r
 
     return a
 
-h = 1.0577336
-x = hr_to_btc(h, d, r)
-print(x)
-
-def btc_to_btc(a, d, r):
+def btc_to_hr(a, d, r):
     ''' int, int -> float 
     Calculates the expected mine assets a according to a hash rate h
     '''
-    ghr = d * 2**32 / 600
+    ghr = d * 2**32 / (t*10**18)
     hr = (a/r) * ghr
 
     return hr
 
+if __name__ == '__main__':
+    d = 14776367535688  # bits
+    r = 12.5*6*24*7     # bitcoin
+    t = 84600
+    h = 200000
+    print(hr_to_btc(h, d, r))

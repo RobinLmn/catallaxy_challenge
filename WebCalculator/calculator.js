3001d1d76a578630f2eeae990f0f1faeee5009b6
kenzalittlegenius <35221608+kenzalittlegenius@users.noreply.github.com>
2020-01-18 23:24:55 -0500

    checkbox

--- WebCalculator/calculator.js
+++ WebCalculator/calculator.js
@@ -1,12 +1,24 @@
 var d;
 
+var display = function() {
+  var checked = document.getElementById("myCheck").checked;
+  if (checked) {
+    btc_mined.style.display = "none";
+    hashpower.style.display = "block";
+  } else {
+    btc_mined.style.display = "block";
+    hashpower.style.display = "none";
+  }
+};
+
 var calcul = function() {
   var hr = document.getElementById("HashingPower").value;
   var hashrate = parseInt(hr);
-  var bitcoinsmined = document.getElementById("bitcoinsmined");
+  var bitcoinsmined = document.getElementById("BitcoinsMined");
   var rewards = document.getElementById("rewards");
   var checked = document.getElementById("myCheck").checked;
   var btc_mined = document.getElementById("btc_mined");
+  var hashpower = document.getElementById("hashpower");
 
   t = 84600;
 
@@ -31,11 +43,12 @@ var calcul = function() {
   //////////////////////////////////////////////
 
   if (checked) {
-    btc_mined.style.dsplay = "none";
     var resultat1 = hr_to_btc(hashrate);
     console.log(resultat1);
+    document.getElementById("result").innerHTML = resultat1 + "";
   } else {
     var resultat2 = btc_to_hr(bitcoinsmined);
+    document.getElementById("result").innerHTML = resultat2 + "";
     console.log(resultat2);
   }
 };
